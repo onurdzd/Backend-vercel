57e44da1eaafb1d36c895b563fc564dc876d7bd8
onurd <106928484+onurdzd@users.noreply.github.com>
2023-04-04 19:17:53 +0300

    knex install

--- api/server.js
+++ api/server.js
@@ -4,6 +4,11 @@ const server=express()
 
 server.use(express.json())
 
+const projectRouter=require("./projects/projects-router")
+
+server.use("/api/projects",projectRouter)
+
+
 server.get("/api",(req,res,next)=>{
     res.status(200).json({message:"Hoşgeldin"})
 })
---
+++ knexfile.js
@@ -0,0 +1,26 @@
+const sharedConfig = {
+  client: 'sqlite3',
+  migrations: {
+    directory: './data/migrations',
+  },
+  seeds: {
+    directory: './data/seeds',
+  },
+  useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)
+    },
+  },
+}
+
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: { filename: './data/vercel.db3' },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './data/testing.db3' },
+  },
+}
